0f184d34ff15ca7bb54732ba7b8f534f7e24462b
Junep <prepared@inflearn.com>
2023-05-19 12:24:28 +0900

    불필요한 is 메서드 제거

--- refactorings/move-to-class/ing.ts
+++ refactorings/move-to-class/ing.ts
@@ -9,61 +9,22 @@ const car = {
 };
 
 interface TrafficLight {
-  isRed(): boolean;
-  isYellow(): boolean;
-  isGreen(): boolean;
   updateCar(): void;
 }
 
 class Red implements TrafficLight {
-  isRed() {
-    return true;
-  }
-
-  isYellow() {
-    return false;
-  }
-
-  isGreen() {
-    return false;
-  }
-
   updateCar() {
     car.stop();
   }
 }
 
 class Yellow implements TrafficLight {
-  isRed() {
-    return false;
-  }
-
-  isYellow() {
-    return true;
-  }
-
-  isGreen() {
-    return false;
-  }
-
   updateCar() {
     car.drive();
   }
 }
 
 class Green implements TrafficLight {
-  isRed() {
-    return false;
-  }
-
-  isYellow() {
-    return false;
-  }
-
-  isGreen() {
-    return true;
-  }
-
   updateCar() {
     car.drive();
   }
