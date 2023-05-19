4574a267204251081cfa3b856b5ab5be0bd02409
Junep <prepared@inflearn.com>
2023-05-19 09:21:24 +0900

    클래스로 타입 코드 대체

---
+++ refactorings/type-to-class/ing.ts
@@ -0,0 +1,26 @@
+const car = {
+  stop() {
+    //
+  },
+
+  drive() {
+    //
+  },
+};
+
+enum TrafficLight {
+  RED,
+  YELLOW,
+  GREEN,
+}
+
+const CYCLE = [TrafficLight.RED, TrafficLight.GREEN, TrafficLight.YELLOW];
+function updateCarForLight(current: TrafficLight) {
+  if (current === TrafficLight.RED) {
+    car.stop();
+  } else {
+    car.drive();
+  }
+}
+
+export {};
