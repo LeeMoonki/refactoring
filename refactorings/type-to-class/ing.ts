53ffbc5c239c1c3495fe4e5ef355cd1acbcbb737
Junep <prepared@inflearn.com>
2023-05-19 09:23:38 +0900

    새로운 클래스

--- refactorings/type-to-class/ing.ts
+++ refactorings/type-to-class/ing.ts
@@ -29,4 +29,46 @@ interface TrafficLight2 {
   isGreen(): boolean;
 }
 
+class Red implements TrafficLight2 {
+  isRed() {
+    return true;
+  }
+
+  isYellow() {
+    return false;
+  }
+
+  isGreen() {
+    return false;
+  }
+}
+
+class Yellow implements TrafficLight2 {
+  isRed() {
+    return false;
+  }
+
+  isYellow() {
+    return true;
+  }
+
+  isGreen() {
+    return false;
+  }
+}
+
+class Green implements TrafficLight2 {
+  isRed() {
+    return false;
+  }
+
+  isYellow() {
+    return false;
+  }
+
+  isGreen() {
+    return true;
+  }
+}
+
 export {};
